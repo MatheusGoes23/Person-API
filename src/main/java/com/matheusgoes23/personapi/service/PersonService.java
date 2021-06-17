e234e60deddb3605f87f6b75cc511cef259537d3
MatheusGoes23 <MatheuGoes23@github.com>
2021-06-17 09:23:39 -0300

    Developed list all method

--- src/main/java/com/matheusgoes23/personapi/service/PersonService.java
+++ src/main/java/com/matheusgoes23/personapi/service/PersonService.java
@@ -7,6 +7,10 @@ import com.matheusgoes23.personapi.mapper.PersonMapper;
 import com.matheusgoes23.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -28,5 +32,12 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
